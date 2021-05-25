4ddf7de5bedd696db41578e0aa8875889f18b015
Francis Behnen <francis.behnen@gmail.com>
2021-05-25 04:31:33 +0200

    Add efficient analyzing method

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from timeit import Timer
-from dataread9test import dataread9test
+import csv
 
-# Good timer reference https://stackoverflow.com/a/24105845/4600952
-print(len(dataread9test()), min(Timer(dataread9test).repeat(repeat=3, number=3)))
+from dataread9test import dataread9test
+from pandas1 import pandas1
+from pandas2 import pandas2
+from pandas3 import pandas3
+from pandas4 import pandas4
+from pandas5 import pandas5
+
+
+# Count max columns, was gonna add this as method argument, but doesn't work with timeit
+columns = 0
+with open('data_test', 'r') as f:
+    csv_f = csv.reader(f, delimiter=' ')
+    max = 0
+    for row in csv_f:
+        max = len(row) if len(row) > max else max
+    columns = max
+print(columns)
+
+if __name__ == '__main__':
+    # Good timer reference https://stackoverflow.com/a/24105845/4600952
+    print(len(dataread9test()), min(Timer(dataread9test).repeat(repeat=3, number=1)))
+    # print(len(pandas1()      ))#, min(Timer(pandas1      ).repeat(repeat=3, number=3)))
+    # print(len(pandas2()      ))#, min(Timer(pandas2      ).repeat(repeat=3, number=3)))
+    # print(len(pandas3()      ))#, min(Timer(pandas3      ).repeat(repeat=3, number=3)))
+    # print(len(pandas4()      ))#, min(Timer(pandas4      ).repeat(repeat=3, number=3)))
+    print(len(pandas5()      ))#, min(Timer(pandas5      ).repeat(repeat=1, number=1)))
--- pandas4.py
+++ pandas4.py
@@ -1,18 +1,34 @@
 from functools import reduce
+from concurrent.futures import ThreadPoolExecutor
+
+import dask
 from pandas import pandas as pd
+import dask.dataframe as ddf
+import dask.bag as db
 
-bi, ii, oi = 0, 2, 3
+bi, ii, oi, columns = 0, 2, 3, 3882
 
 
-def pandas3(columns):
-    df = pd.read_csv('data_test', sep=' ', names=list(range(columns)))
-    df['dict2'] = df.apply(lambda v: {
-                            v[x + int(v[oi])]: (int(v[bi]), float(v[x]))
-                            for x in range(4 + int(v[ii]), 4 + int(v[ii]) + int(v[oi]))
-                            if not (v[x + int(v[oi])] in v.iloc[4: 4 + int(v[ii])].values)
-                            }, axis=1)
-    d = {}
-    for cell in df['dict2'].values:
-        d.update(cell)
-
-    return d
+def pandas4():
+    with dask.config.set(pool=ThreadPoolExecutor(6)):
+        # Dask is lazy, so creating this variable shouldn't matter
+        df = pd.read_csv('data_test', sep=' ', names=list(range(columns)), dtype={k: str for k in range(columns)})
+        # print(df.dtypes)
+        # print(df.memory_usage().sum() / 1e3)
+        # dtype = pd.SparseDtype(str)
+        # df = df.astype(dtype)
+        # print(df.dtypes)
+        # print(df.memory_usage().sum() / 1e3)
+        # return db.from_delayed(df.apply(lambda v: {
+        #                    v[x + int(v[oi])]: (int(v[bi]), float(v[x]))
+        #                    for x in range(4 + int(v[ii]), 4 + int(v[ii]) + int(v[oi]))
+        #                    if not (v[x + int(v[oi])] in v.iloc[4: 4 + int(v[ii])].values)
+        #                }, axis=1, meta=('v', object))) \
+        #          .fold(lambda acc, b: {**acc, **b}) \
+        #          .to_delayed()
+        return reduce(lambda acc, b: {**acc, **b},
+                       df.apply(lambda v: {
+                           v[x + int(v[oi])]: (int(v[bi]), float(v[x]))
+                           for x in range(4 + int(v[ii]), 4 + int(v[ii]) + int(v[oi]))
+                           if not (v[x + int(v[oi])] in v.iloc[4: 4 + int(v[ii])].values)
+                       }, axis=1).values)
